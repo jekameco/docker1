73e169ffe8db5893eb3ec51673df3d842ecc356f
Jekameco <mesakathe31@gmail.com>
2026-07-25 19:35:15 -0500

    agregar docker composer

--- curso1/index.js
+++ curso1/index.js
@@ -1,4 +1,22 @@
 const express = require('express');
+const { Pool } = require('pg');
+const pool = new Pool({
+    user:  process.env.PGUSER || 'postgres',
+    host: process.env.PGHOST || 'localhost',
+    database: process.env.PGDATABASE || 'postgres',
+    password: process.env.PGPASSWORD || '123456',
+    port: process.env.PGPORT || 5432,
+});
 const app  = express();
-app.get('/',(req,res) => res.send('funcop'));
+app.get('/', async (req,res) => {
+    //res.send('funcionando'));
+    try {
+        const result = await pool.query('SELECT NOW()');
+        res.send(result.rows[0]);
+    } catch (error) {
+        console.error('Error al conectar a la base de datos', error);
+        res.status(500).send('Error al conectar a la base de datos');
+    }
+});
+
 app.listen(3000,()=> console.log('puerto 3000 up '))
